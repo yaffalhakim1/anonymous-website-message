d155b3860a25d109166a972f3bd766ddf96d1575
yaffalhakim1 <yafi.tamfan08@gmail.com>
2023-06-04 11:04:49 +0700

    add repo link

--- components/PublicForm.tsx
+++ components/PublicForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { LogoIcon, IconInfo } from "./base/Icons";
+import { LogoIcon, IconInfo, Github } from "./base/Icons";
 import { Button } from "./base/Button";
 import { supabase } from "../lib/supabaseClient";
 import useSWR from "swr";
@@ -42,17 +42,21 @@ function PublicForm() {
       <div className="flex justify-center mt-5">
         <LogoIcon width="24px" height="24px" className="text-blue-500" />
         <h2 className="ml-2 text-lg font-semibold text-gray-900">
-          Anonymous message
+          <span>
+            <a href="https://github.com/yaffalhakim1/anonymous-website-message">
+              Anonymous message
+            </a>
+          </span>
         </h2>
       </div>
 
       <div className="flex flex-col mt-10">
-        <h1 className="block text-2xl font-normal text-gray-900 mb-2 text-center">
-          Send your secret message to
+        <h1 className="block text-2xl font-normal text-gray-900 mb-5 text-center">
+          Send your secret message here
         </h1>
-        <span className="block text-2xl font-semibold text-gray-900 mb-5 text-center">
-          {"someone"}
-        </span>
+        {/* <span className="block text-2xl font-semibold text-gray-900 mb-5 text-center">
+          {"Me"}
+        </span> */}
       </div>
       <div className="mb-6 flex flex-col px-8 lg:px-96">
         <label className="block mb-2 text-sm font-medium text-black ">
@@ -64,7 +68,7 @@ function PublicForm() {
             id="message"
             rows={5}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-            placeholder="Write your thoughts here..."
+            placeholder="If you have opinions, suggestions, or anything else about me please write here..."
             value={message}
             onChange={(e) => setMessage(e.target.value)}
           ></textarea>
@@ -77,7 +81,7 @@ function PublicForm() {
         <div className="flex mt-3">
           <IconInfo className="mt-0.5 text-blue-500" />
           <p className="md:text-md text-sm text-gray-500 ml-1">
-            {"someone"} will never know who you are.
+            I will never know who you are.
           </p>
         </div>
       </div>
